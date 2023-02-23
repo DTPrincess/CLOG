68e5284a48a1f82fa2409e767737082f23391fef
okxooxoo <okxooxoo@naver.com>
2023-02-24 05:00:20 +0900

    Add bouncyCheckbox Group

--- screens/Add.js
+++ screens/Add.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    TouchableOpacity,
-    TextInput,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
 } from "react-native";
+import BouncyCheckboxGroup, {
+  ICheckboxButton,
+} from "react-native-bouncy-checkbox-group";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { AntDesign } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
@@ -15,308 +18,293 @@ import { FontAwesome } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 
 export default function Add({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.menuBar}>
-                <TouchableOpacity
-                    onPress={() => navigation.navigate("Home")}
-                    style={styles.home}
-                >
-                    <Entypo name="home" size={35} color="white" />
-                </TouchableOpacity>
+  const [seasons] = useState([
+    {
+      id: 0,
+      text: "Spring",
+      fillColor: "white",
+      unfillColor: "black",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      width: "50%",
+      paddingBottom: 5,
+    },
+    {
+      id: 1,
+      text: "Summer",
+      fillColor: "white",
+      unfillColor: "black",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      width: "50%",
+      paddingBottom: 5,
+    },
+    {
+      id: 2,
+      text: "Fall",
+      fillColor: "white",
+      unfillColor: "black",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      width: "50%",
+      paddingBottom: 5,
+    },
+    {
+      id: 3,
+      text: "Winter",
+      fillColor: "white",
+      unfillColor: "black",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      width: "50%",
+      paddingBottom: 5,
+    },
+  ]);
 
-                <TouchableOpacity style={styles.notification}>
-                    <Ionicons name="notifications" size={35} color="white" />
-                </TouchableOpacity>
+  const [clothes] = useState([
+    {
+      id: 0,
+      text: "Top",
+      fillColor: "white",
+      unfillColor: "black",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      width: "50%",
+      paddingBottom: 5,
+    },
+    {
+      id: 1,
+      text: "Bottom",
+      fillColor: "white",
+      unfillColor: "black",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      width: "50%",
+      paddingBottom: 5,
+    },
+    {
+      id: 2,
+      text: "Outer",
+      fillColor: "white",
+      unfillColor: "black",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      width: "50%",
+      paddingBottom: 5,
+    },
+    {
+      id: 3,
+      text: "Acc",
+      fillColor: "white",
+      unfillColor: "black",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      width: "50%",
+      paddingBottom: 5,
+    },
+  ]);
 
-                <TouchableOpacity
-                    onPress={() => navigation.navigate("Info")}
-                    style={styles.user}
-                >
-                    <FontAwesome name="user-circle-o" size={35} color="white" />
-                </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <View style={styles.menuBar}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          style={styles.home}
+        >
+          <Entypo name="home" size={35} color="white" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.notification}>
+          <Ionicons name="notifications" size={35} color="white" />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Info")}
+          style={styles.user}
+        >
+          <FontAwesome name="user-circle-o" size={35} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.header}>
+        <Text style={styles.add}>Add</Text>
+      </View>
+
+      <ScrollView>
+        <View style={styles.body}>
+          <View style={styles.image}>
+            <TouchableOpacity style={styles.addImage}>
+              <AntDesign name="pluscircle" size={60} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.content}>
+            <View style={styles.category}>
+              <Text style={styles.categoryText}>Season</Text>
+              <BouncyCheckboxGroup data={seasons} style={styles.checkbox} />
+            </View>
+            <View style={styles.category}>
+              <Text style={styles.categoryText}>Category</Text>
+              <BouncyCheckboxGroup data={clothes} style={styles.checkbox} />
             </View>
 
-            <View style={styles.header}>
-                <Text style={styles.add}>Add</Text>
+            <View style={styles.category}>
+              <Text style={styles.categoryText}>Name</Text>
+              <TextInput style={styles.textinput} />
             </View>
-
-            <ScrollView>
-                <View style={styles.body}>
-                    <View style={styles.image}>
-                        <TouchableOpacity style={styles.addImage}>
-                            <AntDesign name="pluscircle" size={60} color="black" />
-                        </TouchableOpacity>
-                    </View>
-
-                    <View style={styles.content}>
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}>Season</Text>
-                            <BouncyCheckbox
-                                onPress={() => { }}
-                                fillColor="white"
-                                unfillColor="black"
-                                text="Spring"
-                                textContainerStyle={{
-                                    marginLeft: 5
-                                }}
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-                            <BouncyCheckbox
-                                onPress={() => { }}
-                                fillColor="white"
-                                unfillColor="black"
-                                text="Summer"
-                                textContainerStyle={{
-                                    marginLeft: 5
-                                }}
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-                        </View>
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}> </Text>
-                            <BouncyCheckbox
-                                onPress={() => { }}
-                                fillColor="white"
-                                unfillColor="black"
-                                text="Fall"
-                                textContainerStyle={{
-                                    marginLeft: 5
-                                }}
-                                style={{
-                                    marginLeft: 60
-                                }}
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-                            <BouncyCheckbox
-                                fillColor="white"
-                                unfillColor="black"
-                                text="Winter"
-                                style={{
-                                    marginRight: 10
-                                }}
-                                textContainerStyle={{
-                                    marginLeft: 5
-                                }}
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-                        </View>
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}>Category</Text>
-                            <BouncyCheckbox
-                                onPress={() => { }}
-                                fillColor="white"
-                                unfillColor="black"
-                                textContainerStyle={{
-                                    marginLeft: 5
-                                }}
-                                text="Top"
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-
-                            <BouncyCheckbox
-                                onPress={() => { }}
-                                fillColor="white"
-                                unfillColor="black"
-                                style={{
-                                    marginLeft: 20,
-                                    marginRight: 0
-                                }}
-                                textContainerStyle={{
-                                    marginLeft: 5
-                                }}
-                                text="Bottom"
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-                        </View>
-
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}> </Text>
-                            <BouncyCheckbox
-                                onPress={() => { }}
-                                fillColor="white"
-                                unfillColor="black"
-                                style={{
-                                    marginLeft: 100,
-                                    
-                                }}
-                                textContainerStyle={{
-                                    marginLeft: 5
-                                }}
-                                text="Outer"
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-
-                            <BouncyCheckbox
-                                onPress={() => { }}
-                                fillColor="white"
-                                unfillColor="black"
-                                text="Acc"
-                                style={{
-                                    marginLeft: 0,
-                                    marginRight: 25
-                                }}
-                                textContainerStyle={{
-                                    marginLeft: 5
-                                }}
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-                        </View>
-
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}>Name</Text>
-                            <TextInput style={styles.textinput} />
-                        </View>
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}>Brand</Text>
-                            <TextInput style={styles.textinput} />
-                        </View>
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}>Price</Text>
-                            <TextInput style={styles.textinput} />
-                        </View>
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}>Date</Text>
-                            <TextInput style={styles.textinput} />
-                        </View>
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}>Wear</Text>
-                            <TextInput style={styles.textinput} />
-                        </View>
-                        <View style={styles.category}>
-                            <Text style={styles.categoryText}>Washed</Text>
-                            <BouncyCheckbox
-                                onPress={() => { }}
-                                fillColor="white"
-                                unfillColor="black"
-                                style={{
-                                    marginRight: 170
-                                }}
-                                disableText
-                                textStyle={{
-                                    textDecorationLine: "none",
-                                }}
-                            />
-                        </View>
-                    </View>
-                </View>
-
-                <TouchableOpacity style={styles.plus}>
-                    <AntDesign name="pluscircle" size={60} color="white" />
-                </TouchableOpacity>
-            </ScrollView>
+            <View style={styles.category}>
+              <Text style={styles.categoryText}>Brand</Text>
+              <TextInput style={styles.textinput} />
+            </View>
+            <View style={styles.category}>
+              <Text style={styles.categoryText}>Price</Text>
+              <TextInput style={styles.textinput} />
+            </View>
+            <View style={styles.category}>
+              <Text style={styles.categoryText}>Date</Text>
+              <TextInput style={styles.textinput} />
+            </View>
+            <View style={styles.category}>
+              <Text style={styles.categoryText}>Wear</Text>
+              <TextInput style={styles.textinput} />
+            </View>
+            <View style={styles.category}>
+              <Text style={styles.categoryText}>Washed</Text>
+              <BouncyCheckbox
+                onPress={() => {}}
+                fillColor="white"
+                unfillColor="black"
+                style={{
+                  flex: 1,
+                  marginLeft: 15,
+                }}
+                disableText
+                textStyle={{
+                  textDecorationLine: "none",
+                }}
+                innerIconStyle={{
+                  borderRadius: 0,
+                }}
+                iconStyle={{
+                  borderRadius: 0,
+                }}
+              />
+            </View>
+          </View>
         </View>
-    );
+
+        <TouchableOpacity style={styles.plus}>
+          <AntDesign name="pluscircle" size={60} color="white" />
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "black",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
 
-    menuBar: {
-        //justifyContent: "flex-end",
-        flexDirection: "row",
-        width: "100%",
-        marginTop: 60,
-        paddingHorizontal: 15,
-    },
+  menuBar: {
+    //justifyContent: "flex-end",
+    flexDirection: "row",
+    width: "100%",
+    marginTop: 60,
+    paddingHorizontal: 15,
+  },
 
-    home: {
-        flex: 1,
-        paddingHorizontal: 3,
-    },
+  home: {
+    flex: 1,
+    paddingHorizontal: 3,
+  },
 
-    notification: {
-        paddingHorizontal: 3,
-    },
+  notification: {
+    paddingHorizontal: 3,
+  },
 
-    user: {
-        paddingHorizontal: 3,
-    },
+  user: {
+    paddingHorizontal: 3,
+  },
 
-    header: {
-        marginBottom: 15,
-        paddingLeft: 15,
-    },
+  header: {
+    marginBottom: 15,
+    paddingLeft: 15,
+  },
 
-    add: {
-        fontSize: 48,
-        fontWeight: "600",
-        color: "white",
-    },
+  add: {
+    fontSize: 48,
+    fontWeight: "600",
+    color: "white",
+  },
 
-    body: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  body: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    image: {
-        justifyContent: "center",
-        alignitems: "center",
-        width: 200,
-        height: 200,
-        backgroundColor: "white",
-        borderRadius: 30,
-    },
+  image: {
+    justifyContent: "center",
+    alignitems: "center",
+    width: 200,
+    height: 200,
+    backgroundColor: "white",
+    borderRadius: 30,
+  },
 
-    addImage: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  addImage: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    content: {
-        flex: 1,
-        marginTop: 10,
-    },
+  content: {
+    flex: 1,
+    marginTop: 10,
+  },
 
-    category: {
-        flex: 1,
-        justifyContent: "space-between",
-        alignItems: "baseline",
-        flexDirection: "row",
-        marginHorizontal: 10,
-        marginTop: 10,
-    },
+  category: {
+    flex: 1,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginHorizontal: 10,
+    marginTop: 10,
+  },
 
-    categoryText: {
-        color: "white",
-        fontSize: 25,
-        fontWeight: "600",
-        marginRight: 10,
-    },
+  categoryText: {
+    color: "white",
+    fontSize: 25,
+    fontWeight: "600",
+    marginRight: 10,
+  },
 
-    textinput: {
-        color: "white",
-        fontSize: 16,
-        borderBottomColor: "white",
-        borderWidth: 2,
-        width: 220,
-        paddingBottom: 5,
-        paddingHorizontal: 5,
-    },
+  textinput: {
+    color: "white",
+    fontSize: 16,
+    borderBottomColor: "white",
+    borderWidth: 2,
+    width: 220,
+    paddingBottom: 5,
+    paddingHorizontal: 5,
+  },
 
-    plus: {
-        alignItems: "center",
-        marginTop: 20,
-        marginBottom: 35,
-    },
+  plus: {
+    alignItems: "center",
+    marginTop: 20,
+    marginBottom: 35,
+  },
+
+  checkbox: {
+    width: 220,
+    flexWrap: "wrap",
+  },
 });
